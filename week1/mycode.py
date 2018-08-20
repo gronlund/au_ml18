6ef217784392f7c080f4d0e9399264553e2284f9
Allan Grønlund <jallan@cs.au.dk>
2018-08-20 13:10:44 +0200

    more python intro

---
+++ week1/mycode.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+print('is np = plt:', np==plt)
+
+def main():
+    print('i am not called automatically')
+
+if __name__ == '__main__':
+    main()
